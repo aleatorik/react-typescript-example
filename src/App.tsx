4f5cf6bd497418dec45067f3c249918b2145ba5d
daseulsong <vates0328@gmail.com>
2020-10-03 14:09:04 +0900

    Initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+// interface AppProps {
+//   sendSearchQuery?(): void;
+// }
+
+/* 위의 interface 선언 방식보다 아래의 type으로 선언하면 좋은 이유:
+<< type으로 선언된 props에 마우스 호버 시 그 객체의 형태를 보여준다 >>
+*/
+
+type AppProps = {
+  sendSearchQuery?(): void;
+};
+
+const App: React.FunctionComponent<AppProps> = ({
+  sendSearchQuery = () => undefined,
+}) => {
+  const [searchValue, setSearchValue] = React.useState<string>();
+
+  const onSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const search = () => {
+    sendSearchQuery();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={searchValue}
+        onChange={onSearchInput}
+        name="search"
+        type="text"
+      />
+      <button onClick={search}>Search</button>
     </div>
   );
-}
+};
 
 export default App;
